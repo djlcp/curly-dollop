16dc2a3cf6582a311d055e4020f7a014ce4d6669
arturbartkowicz <artur.bartkowicz1@gmail.com>
2019-10-12 11:31:47 +0100

    update employer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_100310) do
+ActiveRecord::Schema.define(version: 2019_10_12_101826) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 2019_10_12_100310) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "business_name"
+    t.string "profile"
     t.index ["email"], name: "index_employers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_employers_on_reset_password_token", unique: true
   end
