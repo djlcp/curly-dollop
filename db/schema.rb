de9a04a64fdd730bc3c8da4d91aecabc458a7bdb
arturbartkowicz <artur.bartkowicz1@gmail.com>
2019-10-12 12:06:48 +0100

    add table employee_skills

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_101826) do
+ActiveRecord::Schema.define(version: 2019_10_12_104158) do
+
+  create_table "employee_skills", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "employee_id"
+    t.integer "skill_id"
+  end
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
