1f8fac2557cfdbf490034553b23a52a4dbbbc24f
NotUsedBrain <notusedbrain@hotmail.com>
2019-11-06 06:21:30 +0000

    added rich text editor for job posts

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -4,6 +4,7 @@ class JobPosting < ApplicationRecord
   belongs_to :employer
   has_many :feedbacks
   has_many :job_applications
+  has_rich_text :content
 
   accepts_nested_attributes_for :skills
 
