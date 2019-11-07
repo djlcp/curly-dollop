006c6c3be35cd31c4c9a799215dffc1a3960ace9
Axel <apys_91@yahoo.co.uk>
2019-11-07 20:03:01 +0000

    Minor updates

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -2,7 +2,7 @@ class JobApplication < ApplicationRecord
     belongs_to :job_posting
     has_many :employees, through: :job_postings
 
-    accepts_nested_attributes_for allow_destroy: true
+    accepts_nested_attributes_for :job_postings, :skills, allow_destroy: true
 
     validates(:content, presence: true)
 end
