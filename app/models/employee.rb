5076349b7bb3096bce08ef677a1944f41ff09edc
NotUsedBrain <notusedbrain@hotmail.com>
2019-11-14 12:35:53 +0000

    cleaning up <code>

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,8 +4,6 @@ class Employee < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :employee_skills
-  has_many :skills, through: :employee_skills
   has_many :job_postings, through: :job_applications
   has_many :feedbacks
   has_one :profile, dependent: :destroy
--- app/models/employees_skill.rb
+++
@@ -1,4 +0,0 @@
-class EmployeesSkill < ApplicationRecord
-  belongs_to :employee
-  belongs_to :skill
-end
