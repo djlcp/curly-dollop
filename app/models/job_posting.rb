fa0ad07b9763bf7aa3502b82f6a91df68c900b37
NotUsedBrain <notusedbrain@hotmail.com>
2019-11-14 09:37:55 +0000

    added associations

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -1,11 +1,8 @@
 class JobPosting < ApplicationRecord
-  has_many :job_postings_skills
-  has_many :skills, through: :job_postings_skills
   belongs_to :employer
+  belongs_to :profession
   has_many :feedbacks
   has_many :job_applications
-
-  accepts_nested_attributes_for :skills
 
   validates(:content, presence: true)
 end
--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,2 +1,3 @@
 class Profession < ApplicationRecord
+  has_many: job_postings
 end
