c2a31c1b5c05939e4a84b2ae97ec3feb3d40a08a
NotUsedBrain <notusedbrain@hotmail.com>
2019-11-15 09:05:04 +0000

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+    
+  #Passing parameters for submitting the form 
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  
+  def configure_permitted_parameters
+    columns = %i[first_name last_name phone_number business_name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: columns)  
+    devise_parameter_sanitizer.permit(:account_update, keys: columns)   
+  end
   before_action :check_user
 
   def check_user
