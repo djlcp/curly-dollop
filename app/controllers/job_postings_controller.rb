60680b5f64f10c71eca32285b86b0d391d3e8e47
NotUsedBrain <notusedbrain@hotmail.com>
2019-11-17 10:18:49 +0000

    updated controller for job_postings

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -1,11 +1,12 @@
 class JobPostingsController < ApplicationController
+  before_action :find_job_posting, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @job_postings = JobPosting.all
+  end
 
   def index
-    @job_postings = if current_employer
-                      current_employer.job_postings
-                    elsif current_employee
-                      current_employee.job_postings
-                    end
+      @job_postings = JobPosting.all
   end
 
   def new
@@ -13,30 +14,42 @@ class JobPostingsController < ApplicationController
   end
 
   def create
-    @job_posting = JobPosting.new(job_posting_params)
+    @job_posting = JobPosting.new(job_postings_params)
+    
     if @job_posting.save
-      redirect_to root_path
+      redirect_to @job_posting
+    else
+      render "New"
     end
   end
 
   def show
-    @job_posting = JobPosting.find(params[:id])
   end
 
+
   def update
-    @job_posting = JobPosting.find(params[:id])
-    if @job_posting.update_attributes(job_posting_params)
-      redirect_to root_path
+    if @job_posting.update(job_postings_params)
+      redirect_to @job_posting
     else
-      render :edit
+      render "Edit"
     end
+  end
+
+  def destroy
+    @job_posting.destroy
+    redirect_to root_path
   end
 
   private
   
-  def job_posting_params
+  def job_postings_params
     params.require(:job_posting).permit(
-      :title, :content, :start_time, :end_time, :hourly_rate
+      :content, :start_time, :end_time, :hourly_rate, :profession_id
     ).merge(employer_id: current_employer.id)
   end
+
+  def find_job_posting
+    @job_posting = JobPosting.find(params[:id])
+  end
+
 end
