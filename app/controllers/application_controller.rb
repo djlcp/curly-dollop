4f4d42ca2790b46262d01dc2c1fefc386dc4b9ba
Axel <apys_91@yahoo.co.uk>
2019-11-26 20:08:12 +0000

    Further minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,11 @@ class ApplicationController < ActionController::Base
 
   def check_user
     if current_employer
-      flash.clear
+      # flash.clear
       # if you have rails_admin. You can redirect anywhere really
       #redirect_to(rails_employer.dashboard_path) && return
     elsif current_employee
-      flash.clear
+      # flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
       # redirect_to(authenticated_employee_root_path) && return
     else
