40a44598048a7b0248e0c68790f7d68b2c318a1a
NotUsedBrain <notusedbrain@hotmail.com>
2019-11-30 17:45:28 +0000

    changing reg controller to build profile at the same time

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -41,13 +41,13 @@ class Employees::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :profession_id])
+  end
+ 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
