240e1ab1b026334cbdb49d8e9390f3822798b162
NotUsedBrain <notusedbrain@hotmail.com>
2019-12-01 14:10:55 +0000

    removed flashes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,19 +8,20 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_permitted_parameters
-    added_attrs = [:email, :password, :password_confirmation, :remember_me, :last_name, :first_name, :profession_id]
-    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)  
-    devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)   
+      devise_parameter_sanitizer.permit(:sign_up) { |u|
+        u.permit(:email, :password, :password_confirmation, :employee_profile_attributes => [:first_name, :last_name])
+      }
+    columns = %i[email password password_confirmation]
+    devise_parameter_sanitizer.permit(:account_update, keys: columns)   
   end
+
   before_action :check_user
 
   def check_user
     if current_employer
-      flash.clear
       # if you have rails_admin. You can redirect anywhere really
       #redirect_to(rails_employer.dashboard_path) && return
     elsif current_employee
-      flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
       # redirect_to(authenticated_employee_root_path) && return
     else
