3c8b684fa5436fa5078f7f1883a0815525c14f0a
Axel <apys_91@yahoo.co.uk>
2019-12-03 12:18:36 +0000

    Job_posting employee name link_to accept/reject

--- app/controllers/job_postings/job_applications_controller.rb
+++ app/controllers/job_postings/job_applications_controller.rb
@@ -1,13 +1,13 @@
 module JobPostings
   class JobApplicationsController < ApplicationController
     before_action :set_job_posting
+    before_action :set_job_application
     before_action :check_user
 
     def create
-      @job_application = @job_posting.job_applications.new(employee: current_employee, status: :applied)
+      @job_application = @job_posting.job_applications.new(employee: current_employee, status: 2)
       if already_applied?
-        redirect_to root_path
-        flash[:notice] = "You can't apply more than once"
+        redirect_to root_path, notice: "You can't apply more than once"
       else 
         @job_application.save
         # EmployerMailer.job_application_email(@job_posting.employer, @job_application).deliver
@@ -16,20 +16,65 @@ module JobPostings
     end
 
     def show
-      @job_application = JobApplication.all
-      @job_posting = JobPosting.all
     end
 
+    def index
+    end
+
+    def accept
+      @job_application.update_attributes(status: 0)
+    end
+
+    def reject
+      @job_application = @job_application.update_attributes(params[status: 1])
+    end
+
+    # Change status of job_application
+    # def accept_application
+    #   @job_application = JobApplication.find(params[:id])
+    #   @job_application.update(status: params[:status = 1])
+    #   redirect_to @job_posting, notice: 'Application has been Accepted. Employee will be notified.'
+    # end
+    #   if params[status: :accepted]== 'Accepted'
+    #     @job_application.update_attributes(:status => 0)
+        
+    #     redirect_to :back, flash: {notice: "Application has been Accepted. Employee will be notified."}
+
+    #   elsif params[status: :rejected]== 'Rejected'
+    #     @job_application.update_attributes(:status => 1)
+    #     redirect_to :back, flash: {notice: "Application has been Rejected. Employee will be notified."}
+
+    #   elsif params[status: :applied]== 'Applied'
+    #     @job_application.update_attributes(:status => 3)
+    #     redirect_to root_path, flash: {notice: "Waiting for Response"}
+    #   end
+    # end
+
     private
+
+    def job_applications_params
+      params.permit(:status, :job_posting_id, :employee_id, :employer_id)
+      # .merge(employee_id: current_employee.id)
+    end  
 
     def set_job_posting
       @job_posting = JobPosting.find(params[:job_posting_id])
     end
 
+    def set_job_application
+      @job_application = JobApplication.find(params[:id])
+    end
+
     def already_applied?
-      JobApplication.where(employee_id: current_employee.id, job_posting_id:
+      @job_application = JobApplication.where(employee_id: current_employee.id, job_posting_id:
       params[:job_posting_id]).exists?
     end
+    
+
+    # def already_accepted?
+    #   JobApplication.where(employee_id: current_employee.id, job_posting_id:
+    #   params[:job_posting_id]).exists?
+    # end
 
   end
 end
