017237ce0473a3b4717740b73b6df0f0242f54cf
Tiago Soares <tiago.soares@avantry.io>
2019-12-10 14:35:29 +0100

    corrected mailers

--- app/mailers/employer_mailer.rb
+++ app/mailers/employer_mailer.rb
@@ -1,5 +1,4 @@
 class EmployerMailer < ApplicationMailer
-  default :from => 'Shiift__@hotmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def employer_signup_email(employer)
