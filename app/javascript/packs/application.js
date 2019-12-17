a7e64a7e568341385c237f6812ec7faf99dcab7c
arturbartkowicz <artur.bartkowicz1@gmail.com>
2019-12-17 22:02:51 +0000

    Edit title changed. different choose button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 
 import "../src/cocoon"
 import "../src/datepicker"
+import "../custom/_form"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,4 +21,6 @@ import "../src/datepicker"
 
 require("trix")
 require("@rails/actiontext")
+
+
 
